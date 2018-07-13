b0138609a93a4908b86c1ef0a13c577c29011b46
Michael Kitzan <mkitzan45@gmail.com>
2018-07-13 14:45:15 -0700

    Record screens added
    The saving part is rough. Will be polishing it up by the end of the day.

--- prototype.py
+++ prototype.py
@@ -1,48 +1,77 @@
 import re
 from kivy.app import App
+from kivy.lang.builder import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button, Label
 from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.screenmanager import ScreenManager, Screen
 
    
-class Prototype(BoxLayout):
-    instruments = ["Guitar", "Ukulele", "Mandolin", "Banjo"]
-    songlist = None
-    
+class Landing(Screen):
     def build_songlist(self):
-        self.songlist = []
+        self.remove_widget(self.ids.songlist_view)
+        songlist = []
     
         with open("songlist.txt", "r") as infile:
             for line in infile:
                 match = re.match("^(.*)::(.*)\n$", line)
-                self.songlist += [Button(text=match.group(1)+" ["+match.group(2)+"]", background_normal="icons/bg.png", text_size=[580, None], color=[0, 0, 0, 1], valign="middle", halign="left", on_click=self.song, font_size=24, size_hint_y=None, height=50)]
+                songlist += [Button(text=match.group(1)+" ["+match.group(2)+"]", background_normal="icons/bg.png", text_size=[580, None], color=[0, 0, 0, 1], \
+                                         valign="middle", halign="left", on_click=self.song, font_size=24, size_hint_y=None, height=50)]
                 
-        self.songlist.sort(key=lambda btn: btn.text.capitalize())
+        songlist.sort(key=lambda btn: btn.text.capitalize())
         alpha = ""
         
-        for btn in self.songlist:
+        for btn in songlist:
             if btn.text[0].capitalize() != alpha:
                 alpha = btn.text[0].capitalize()
                 self.ids.songlist_view.add_widget(Label(text="", size_hint_y=None, height=20))
                 self.ids.songlist_view.add_widget(Label(text=alpha, text_size=[580, None], valign="middle", halign="left", font_size=32, color=[0, 0, 0, 1], size_hint_y=None, height=50))
             self.ids.songlist_view.add_widget(btn)
+          
+            
+    def song():
+        pass
         
         
-    def record(self):
-        # create new screens for the stop -> processing -> save
-        pass
+class Record(Screen):
+    hold = None
+    
+    def reset(self):
+        if self.hold is not None:
+            self.ids.grid.remove_widget(self.ids.grid.children[0])
+            self.ids.grid.add_widget(self.hold)
+        
+        self.ids.element.background_normal="icons/stop-normal.png"
+        self.ids.element.background_down="icons/stop-down.png"
 
 
-    def song(self, instance):
-        pass
-
+    def pressed(self):
+        if self.ids.element.background_normal == "icons/stop-normal.png":
+            self.ids.element.background_normal="icons/buffer.png"
+            self.ids.element.background_down="icons/buffer.png"
+        else:
+            self.hold = self.ids.element
+            self.ids.grid.remove_widget(self.ids.grid.children[0])
+            self.ids.grid.add_widget(SaveInput())
+            
+        
+class Song(Screen):
+    pass
+    
+    
+class SaveInput(BoxLayout):
+    pass
+    
 
 class PrototypeApp(App):
     def build(self):
-        pt = Prototype()
-        pt.build_songlist()
-        return pt
+        sm = ScreenManager()
+        sm.add_widget(Landing(name="landing"))
+        sm.children[0].build_songlist()
+        sm.add_widget(Record(name="record"))
+        
+        return sm
 
 
 if __name__ == "__main__":
